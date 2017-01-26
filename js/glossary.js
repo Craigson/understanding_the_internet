641b195a6bbd751549ef70562c292b3bb9e0bedc
Craigson <craigpick@gmail.com>
2017-01-26 17:48:16 -0500

    initial commit

---
+++ js/glossary.js
@@ -0,0 +1,11 @@
+var glossary = {
+	
+	"http" : {
+		definition: "Hypertext Transfer Protocol",
+		keywords: ["world wide web", "communication", "data transfer", "HTTP"],
+		description: "Initiated by Tim Berners-Lee at CERN in 1989.  HTTP is a request-response protocol based on a client-server model in computing.\
+		A common use case is a web browser (client) requesting information from a server ( eg. Google )",
+		use_case: ""
+	}
+	
+};
---
+++ js/script.js
@@ -0,0 +1,26 @@
+console.log(glossary);
+
+console.log('herro');
+
+
+function createEntry(topic, def, keywords, description, use_case ){
+
+	$('#registration_pi_container').append(`
+		      <div class="row" class="reg_pi">
+		        
+		            <div class="col-md-2">
+		              <button id="${dev}_pi_status" type="button" class="btn btn-danger">Disconnected</button>
+		            </div>
+		            <div class="col-md-2">
+		             <div id = "${dev}_ip" class = "dynamic">${ip} </div>
+		            </div>
+		            <div class="col-md-2">
+		             <div id = "${dev}_device" class = "dynamic">${dev}</div>
+		            </div>
+		            <div class="col-md-6">
+		              <div id="${dev}_pi_commit" class = "dynamic">unknown</div>
+		            </div>
+		        </div> <!-- end of row -->
+	`);
+
+}

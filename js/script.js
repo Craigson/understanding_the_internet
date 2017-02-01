bb8a9942a08db68fec432506d0bdc937b5b6d451
Craigson <craigpick@gmail.com>
2017-02-01 16:38:05 -0500

    adding day _6

--- js/script.js
+++ js/script.js
@@ -1,21 +1,67 @@
 /* TODO:
 - search suggestions
-- 
+-
 - Expand acronym on hover
 - Data visualization showing connections
-- create a footer that shows the path you've taken || a popup that remembers your original place so you can click 'back'
-- support mixed cases for dynamic linking
-- got some regex work to do RE searching within plurals etc.
+- create a footer that shows the path you've taken
+- support mixed cases for dynamic linking || got some regex work to do RE searching within plurals etc.
+- Create a 'learning tree' by number of mentions (eg. the most pertinent topic is presented first.
 */
 
-// hide the back button by default
+// hide the back button and search results bar by default
 $('#back_button').hide();
+$('#search_results').hide();
 
 // Keep track of the positions ( for scrolling ) of each of the topics
 var topicPositions = mapScrollPositions();
 
+// create a js object that will allow for search
+var searchableObject = createSearchableObject();
+
+var matchedTerms = [];
+showSearchBar = false;
+
 // link any mention of topic inside a description to the actual topic
 createContentLinks();
+
+$('#searchbar').keyup(handleChange);
+
+// function handleChange(e){
+// 	console.log( $('#searchbar').val() );
+// }
+
+function handleChange(e){
+
+	// show the search bar as the user types
+	if( showSearchBar == false ) {
+		showSearchBar = true;
+		$("#search_results").show();
+		console.log('showing searchbar');
+	}
+
+	console.log( $("#searchbar").val() );
+
+	var searchTerm = ($("#searchbar").val());
+
+	// loop through the searchable object
+	for (var i = 0; i < searchableObject.length; i++){
+		if ( searchableObject[i].indexOf(searchTerm) != -1 )
+		{
+			console.log("found a match");
+			console.log(searchableObject[i]);
+
+			matchedTerms.push(searchableObject[i])
+		}
+	}
+
+	if ($("#searchbar").val().length == 0){
+		showSearchBar = false;
+		$("#search_results").hide();
+	}
+
+	console.log(showSearchBar);
+
+}
 
 var scrollPosition; // global variable for storing the scroll position when a link is clicked
 
@@ -71,7 +117,7 @@ function createContentLinks(){
 
 				// search through the html and replace the html for the reference topic
 				// with a clickable link to the reference topic
-				
+
 				var topicLink = referenceTopic + "_link";
 				var div = "<span class=\"clickable_link\">" + referenceTopic + "</span>";
 
@@ -79,6 +125,10 @@ function createContentLinks(){
 				var regex = new RegExp("\\b"+referenceTopic+"\\b", "gim");
 				glossaryContainer.children[j].children[3].innerHTML = glossaryContainer.children[j].children[3].innerHTML.replace(regex, div);
 
+				console.log('reference topic: ' + referenceTopic);
+				console.log('section: ' + glossaryContainer.children[j].children[1].innerHTML);
+				console.log(glossaryContainer.children[j].children[3].innerHTML);
+				console.log('\n-----------------------\n');
 				// handle the case where there used as a plural
 
 				// var pluralTopic = referenceTopic + "s";
@@ -89,7 +139,7 @@ function createContentLinks(){
 
 	   		}
 
-	   	} 
+	   	}
 	}
 }
 
@@ -103,11 +153,11 @@ function mapScrollPositions(){
 
 	// create an assosciative array that maps the topic to its scroll position
 	for (var i = 0; i < glossaryContainer.children.length; i++){
-
+		console.log(i);
 		// get the topic name
 		var topic = glossaryContainer.children[i].children[1].innerHTML;
 		var tagId = glossaryContainer.children[i].id;
-		
+
 		// get the scroll position
 		var position = $("#" + tagId).offset().top-20;
 
@@ -120,3 +170,21 @@ function mapScrollPositions(){
 	return obj;
 }
 
+
+function createSearchableObject(){
+
+	var obj = [];
+
+	var glossaryContainer = document.getElementById('glossary-container');
+
+	for (var i = 0; i < glossaryContainer.children.length; i++){
+
+		var topic = glossaryContainer.children[i].children[1].innerHTML;
+		var lowerTopic = topic.toLowerCase();
+		obj.push(lowerTopic);
+	}
+
+	console.log(obj);
+	return obj;
+
+}
